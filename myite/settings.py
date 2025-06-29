98f696da2330bb08ea6595b9e6d3ecb9967256fa
Artur Bezeliuk <buartur12@gmail.com>
2025-06-29 19:54:42 +0300

    Add static files

--- myite/settings.py
+++ myite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q3zw!ab(s4vgi26^q1=9u+#7x9qtmd^_)^3p8pfpx9^d33-5hu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['mygame-5.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['mygame-6.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -118,13 +118,17 @@ LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
 
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [BASE_DIR / 'Ite' / 'static']
+
