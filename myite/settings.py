4714df9df015be048e680f004c367867eedefa61
Artur Bezeliuk <buartur12@gmail.com>
2025-06-29 19:38:21 +0300

    Fix ALLOWED_HOSTS for Render deployment

--- myite/settings.py
+++ myite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q3zw!ab(s4vgi26^q1=9u+#7x9qtmd^_)^3p8pfpx9^d33-5hu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mygame-3.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
