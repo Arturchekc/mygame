83f8867d7d1c19b7b6151cbb1bd1f3969206f8e4
Artur Bezeliuk <buartur12@gmail.com>
2025-06-30 12:09:01 +0300

    last

--- myite/settings.py
+++ myite/settings.py
@@ -74,7 +74,9 @@ WSGI_APPLICATION = 'myite.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
