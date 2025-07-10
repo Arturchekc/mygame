599dec7024d38990cfae2dee76ed40c5e1f72656
Artur Bezeliuk <buartur12@gmail.com>
2025-07-10 10:20:11 +0300

    robots.txt

--- Ite/urls.py
+++ Ite/urls.py
@@ -1,6 +1,6 @@
 import os
-
 from django.urls import path
+from django.views.generic import TemplateView
 from myite import settings
 from . import views
 from django.views.static import serve
@@ -12,5 +12,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('post_comment/', views.post_comment, name='post_comment'),
     path('sitemap.xml', views.sitemap),
-
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
